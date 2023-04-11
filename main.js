e71df8cefbc518a3fa22164e69acbe138cd744ba
Andres Cerquera <acerquera7v7@gmail.com>
2023-04-11 16:29:29 -0500

    Unión del carrito de compras y solucion de interrupcion de interfaces

--- main.js
+++ main.js
@@ -2,15 +2,45 @@ const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileMenuIcon = document.querySelector('.menu');
+const shoppingCartMenu = document.querySelector('.product-detail');
+const shoppingCartIcon = document.querySelector('.navbar-cart');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 mobileMenuIcon.addEventListener('click', toggleMobileMenu);
+shoppingCartIcon.addEventListener('click', toggleShoppingCart);
 
 function toggleDesktopMenu() {
+    const isShoppingCartClosed = shoppingCartMenu.classList.contains('inactive');
+    
+    if (!isShoppingCartClosed) {
+        shoppingCartMenu.classList.add('inactive');
+    }
+
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    console.log('Click');
+    const isShoppingCartClosed = shoppingCartMenu.classList.contains('inactive');
+    
+    if (!isShoppingCartClosed) {
+        shoppingCartMenu.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingCart() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    shoppingCartMenu.classList.toggle('inactive');
 }
