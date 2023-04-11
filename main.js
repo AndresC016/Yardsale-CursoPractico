120685eedea103c71ca51d6dac866e8a7f04cc07
Andres Cerquera <acerquera7v7@gmail.com>
2023-04-10 20:29:26 -0500

    Unión del menu de mobile y ajuste a la resolucion de pantalla en los menu

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-navEmail.addEventListener('click', toggleDesktopMenu)
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenuIcon = document.querySelector('.menu');
+
+navEmail.addEventListener('click', toggleDesktopMenu);
+mobileMenuIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    console.log('Click');
+    mobileMenu.classList.toggle('inactive');
 }
